886aa1ea948135eeaf22ca7afff80c7d121d45da
David Hull <david.hull37@gmail.com>
2020-07-19 17:54:05 -0500

    Added item class for item creation and inventory to player

--- src/com/slenderman/actors/Player.java
+++ src/com/slenderman/actors/Player.java
@@ -1,7 +1,13 @@
 package com.slenderman.actors;
 
+import java.util.*;
+
 public class Player {
-  public String state = "alive";
+  private String state = "alive";
+  private String currentSceneName = "forest";
+
+  private List<Item> inventory = new ArrayList<>();
+
 
   /*
    * =============================================
@@ -17,7 +23,49 @@ public class Player {
    * =============================================
    */
 
-  //TODO: write an inventory method
+  public void addItemToInventory(Item... items) {
+    inventory.addAll(Arrays.asList(items));
+    for (Item item : items) {
+      item.setCurrentScene(this.currentSceneName);
+      inventory.add(item);
+
+      System.out.printf("What a prize! You have added %s to your inventory.\n",
+        item.getItemName());
+    }
+  }
+
+  public void dropItemFromInventory(Item item) {
+    Item returnableItem;
+    if (inventory.contains(item)) {
+      item.setCurrentScene(this.currentSceneName);
+
+      int indexOfItem = inventory.indexOf(item);
+      returnableItem = inventory.get(indexOfItem);
+
+      inventory.remove(indexOfItem);
+
+      System.out.printf("You dropped %s from your inventory.\n", returnableItem.getItemName());
+    } else {
+      System.out.println("It doesn't look like you have that item in your " +
+                         "inventory.\n");
+    }
+  }
+
+  /**
+   * Changes the currentScene field for all items in inventory to Player's
+   * current scene location
+   */
+  public void changeInvItemsLocation() {
+    inventory.forEach(item -> item.setCurrentScene(this.currentSceneName));
+  }
+
+  /**
+   * Changes the currentScene field for all items in inventory to sceneName
+   * @param sceneName
+   */
+  public void changeInvItemsLocation(String sceneName) {
+    inventory.forEach(item -> item.setCurrentScene(sceneName));
+  }
 
   /*
    * =============================================
@@ -30,8 +78,25 @@ public class Player {
     this.state = state;
   }
 
+  public void setInventory(List<Item> inventory) {
+    this.inventory = inventory;
+  }
+
+  public void setCurrentSceneName(String currentSceneName) {
+    this.currentSceneName = currentSceneName;
+  }
+
   // GET METHODS
   public String getState() {
     return state;
   }
+
+  public Collection<Item> getInventory() {
+    return Collections.unmodifiableCollection(inventory);
+  }
+
+  public String getCurrentSceneName() {
+    return currentSceneName;
+  }
+
 }
