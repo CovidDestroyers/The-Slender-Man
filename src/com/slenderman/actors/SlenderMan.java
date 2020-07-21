45f601d589367c969420970a7557826a64a27198
bmyers624 <b.myers624@gmail.com>
2020-07-21 13:13:24 -0700

    little bit more updates

--- src/com/slenderman/actors/SlenderMan.java
+++ src/com/slenderman/actors/SlenderMan.java
@@ -4,7 +4,8 @@ import com.slenderman.scenes.Scene;
 
 public class SlenderMan {
   public static void killPlayer() {
-    System.out.println(Scene.ANSI_RED + "The screeching overpowers all of your senses, you become paralyzed... \n" +
+    System.out.println("---------------------------------------------------------------------------------------------------\n" +
+      Scene.ANSI_RED + "The screeching overpowers all of your senses, you become paralyzed... \n" +
       "SlenderMan emerges from the shadows and wraps his long slimy fingers around your neck and... \n" +
       " ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄       ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄ \n" +
       "▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌     ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌\n" +
