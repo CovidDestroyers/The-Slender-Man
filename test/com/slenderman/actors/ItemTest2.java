cb98675d177f7dce912b732d11505d8dee73cf14
bmyers624 <b.myers624@gmail.com>
2020-07-29 07:46:59 -0700

    updates

--- test/com/slenderman/actors/ItemTest2.java
+++ test/com/slenderman/actors/ItemTest2.java
@@ -1,28 +1,29 @@
 package com.slenderman.actors;
 
-import com.slenderman.inventory.Item;
+//import com.slenderman.inventory.Item;
+import com.slenderman.actors.Item;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-class ItemTest2 {
-    Item item= new Item("itemName", "homeScene", "purpose", "house", false,
-                        "otherItem", "whatDoesOtherItemDo", "itemRevealed");
-
-    @org.junit.jupiter.api.DynamicTest
-    void testPrintAsciiArt(){
-        item.printAsciiArt();
-    }
-
-    @Test
-    void testIsInPlayerInventory(){
-       // Boolean result = item.isInPlayerInventory();
-        // Assertions.assertEquals(Boolean.TRUE, result);
-    }
-
-    @Test
-    void testToString(){
-        String result = item.toString();
-        Assertions.assertEquals("replaceMeWithExpectedResult", result);
-    }
-}
+//class ItemTest2 {
+//    Item item= new Item("itemName", "homeScene", "purpose", "house", false,
+//                        "otherItem", "whatDoesOtherItemDo", "itemRevealed");
+//
+//    @org.junit.jupiter.api.DynamicTest
+//    void testPrintAsciiArt(){
+//        item.printAsciiArt();
+//    }
+//
+//    @Test
+//    void testIsInPlayerInventory(){
+//       // Boolean result = item.isInPlayerInventory();
+//        // Assertions.assertEquals(Boolean.TRUE, result);
+//    }
+//
+//    @Test
+//    void testToString(){
+//        String result = item.toString();
+//        Assertions.assertEquals("replaceMeWithExpectedResult", result);
+//    }
+//}
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
