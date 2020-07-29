a48fe7ff0b3dd404b60052d5eacea773e74acde8
takuhiro55 <takuhiro55@gmail.com>
2020-07-29 09:55:23 -0700

    Readded tests

---
+++ test/com/slenderman/actors/PlayerTest.java
@@ -0,0 +1,95 @@
+package com.slenderman.actors;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+* Player Tester.
+*
+* @author <Authors name>
+* @since <pre>Jul 27, 2020</pre>
+* @version 1.0
+*/
+public class PlayerTest {
+
+@Before
+public void before() throws Exception {
+}
+
+@After
+public void after() throws Exception {
+}
+
+/**
+*
+* Method: addItemToInventory(Item... items)
+*
+*/
+@Test
+public void testAddItemToInventoryItems() throws Exception {
+//TODO: Test goes here...
+}
+
+/**
+*
+* Method: dropItemFromInventory(Item item)
+*
+*/
+@Test
+public void testDropItemFromInventory() throws Exception {
+//TODO: Test goes here...
+}
+
+/**
+*
+* Method: changeInvItemsLocation()
+*
+*/
+@Test
+public void testChangeInvItemsLocation() throws Exception {
+//TODO: Test goes here...
+}
+
+/**
+*
+* Method: changeInvItemsLocation(String sceneName)
+*
+*/
+@Test
+public void testChangeInvItemsLocationSceneName() throws Exception {
+//TODO: Test goes here...
+}
+
+/**
+*
+* Method: setState(String state)
+*
+*/
+@Test
+public void testGetSetState() throws Exception {
+//TODO: Test goes here...
+}
+
+/**
+*
+* Method: setInventory(List<Item> inventory)
+*
+*/
+@Test
+public void testGetSetInventory() throws Exception {
+//TODO: Test goes here...
+}
+
+/**
+*
+* Method: setCurrentSceneName(String currentSceneName)
+*
+*/
+@Test
+public void testGetSetCurrentSceneName() throws Exception {
+//TODO: Test goes here...
+}
+
+
+}
---
+++ test/com/slenderman/scenes/FieldTest.java
@@ -0,0 +1,9 @@
+package com.slenderman.scenes;
+
+import org.junit.jupiter.api.Test;
+
+public class FieldTest {
+    @Test
+    void enter() {
+    }
+}
