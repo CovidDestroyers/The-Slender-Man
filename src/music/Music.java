75ebf94512866a027cffea4b0af20beb1ce6ee49
tapantriv <tapan_trivedi_4u@yahoo.co.in>
2020-08-03 13:16:43 -0700

    now music will continuous loop

--- src/music/Music.java
+++ src/music/Music.java
@@ -9,20 +9,6 @@ import java.net.URL;
 
 public class Music {
 
-
-  // variable
-  private boolean isForestMusic = false;
-//  Constructor
-
-  public Music(boolean isForestMusic) {
-    this.isForestMusic = isForestMusic;
-  }
-
-  public Music() {
-
-  }
-
-
   public static void forestMusic(File file) throws Exception {
 //    gardenMusic =true;
     //when user/Gamer enter in Forestmusic will play
@@ -30,38 +16,13 @@ public class Music {
 //    AudioInputStream ais = AudioSystem.getAudioInputStream(file);
 //    clip.open(AudioSystem.getAudioInputStream(file));
 //    clip.loop(Clip.LOOP_CONTINUOUSLY-1);
+    AudioInputStream audioInput=AudioSystem.getAudioInputStream(file);
     Clip clip = AudioSystem.getClip();
-    clip.open(AudioSystem.getAudioInputStream(file));
+    clip.open(audioInput);
     clip.start();
-
-    Thread.sleep(clip.getMicrosecondLength()/1000);
-
-
-
-//    clip.open(ais);
-
-
-
-  }
-  public static void gameStartMusic() throws Exception {
-    //when user/Gamer enter in garden relex music will play
-    URL url = new URL("https://www.videomaker.com/sites/videomaker.com/files/downloads/free-sound-effects/Free_Timpani_6027_73_1.wav");
-    Clip clip = AudioSystem.getClip();
-
-    AudioInputStream ais = AudioSystem.getAudioInputStream(url);
-    clip.open(ais);
+    //Below thing will loop continuous
     clip.loop(Clip.LOOP_CONTINUOUSLY);
 
-
-  }
-  //getter and setter
-
-  public boolean isForestMusic() {
-    return isForestMusic;
-  }
-
-  public void setForestMusic(boolean forestMusic) {
-    isForestMusic = forestMusic;
   }
 
 
