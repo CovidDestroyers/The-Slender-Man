cbf862b38e4b47a3fba895f8622cc1c08a85b58d
RJ <52873153+tupleHunden@users.noreply.github.com>
2020-08-06 09:22:07 -0700

    Refactored SlenderMan

--- src/com/slenderman/actors/SlenderMan.java
+++ src/com/slenderman/actors/SlenderMan.java
@@ -1,25 +1,14 @@
 package com.slenderman.actors;
 
-//import com.slenderman.scenes.LoseGameScene;
 import com.slenderman.game.Game;
-import com.slenderman.scenes.Scene;
 
 public class SlenderMan {
   public static boolean isGameDone = false;
   public static int completions = 0;
 
-  public static void killPlayer() throws InterruptedException {
+  public static void killPlayer() {
     Game.isPlayerAlive = false;
     isGameDone = true;
-//    System.out.println("---------------------------------------------------------------------------------------------------\n" +
-//      "The screeching overpowers all of your senses, you become paralyzed... \n" +
-//      "SlenderMan emerges from the shadows and wraps his long slimy fingers around your neck and... \n" +
-//      "       __                  __   __   __      \n" +
-//      " /  | /  | /  |      /    /  | /    /     /  \n" +
-//      "(___|(   |(   |     (    (   |(___ (___  (   \n" +
-//      "    )|   )|   )     |   )|   )    )|     |   \n" +
-//      " __/ |__/ |__/      |__/ |__/  __/ |__   _   \n" +
-//      "                                             ");
   }
 
   public static void moveCloserToPlayer() {
